4ea814a715506f88e53dda1c6b145ae71e0d9edd
Kanishk Gandhi <kanishk050296@gmail.com>
2022-01-07 12:40:17 -0800

    change env to rust

--- src/test_solvers.py
+++ src/test_solvers.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     def evaluate_solver(checkpoint, dataset):
         model = torch.load(checkpoint, map_location=device)
         model.to(device)
-        env = environment.RacketEnvironment("http://127.0.0.1:6007", "equations-ct")
+        env = environment.RustEnvironment("equations-ct")
         n_problems = train_dataset.n_problems  # How many problems to use.
         max_steps = 30  # Maximum length of an episode.
         beam_size = 2  # Size of the beam in beam search.
