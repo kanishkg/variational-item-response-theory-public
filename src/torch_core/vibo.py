4f61ce8da6279dfa0775216b0850747b95d284c2
Kanishk Gandhi <kanishk050296@gmail.com>
2022-01-06 17:35:34 -0800

    add plotting

--- src/torch_core/vibo.py
+++ src/torch_core/vibo.py
@@ -626,6 +626,8 @@ if __name__ == "__main__":
                 if args.dataset == 'jsonstep':
                     model_name = "Side-info A-VIBO"
                 print(f'{{ "seed": {args.seed}, "model": "{model_name}", "missing_perc": {args.artificial_missing_perc}, "accuracy": {missing_imputation_accuracy} }},')
+                with open('results_algebra.csv', 'a') as f:
+                    f.write(f'{args.seed}, {model_name}, {missing_perc}, {args.artificial_missing_perc}, {missing_imputation_accuracy}')
                 sys.exit(0)
                 print(f'Missing Imputation Accuracy from samples: {missing_imputation_accuracy}')
 
