1600c0d844da635eb7af2ffd63557b6c2dadba9c
Kanishk Gandhi <kanishk050296@gmail.com>
2022-01-13 13:26:15 -0800

    add feat normalize

--- src/torch_core/models.py
+++ src/torch_core/models.py
@@ -1186,6 +1186,8 @@ class ConpoleEncoder(nn.Module):
             [environment.State([self.problems[i]], [], 0) for i in item_index.flatten()]).detach()
 
         if self.params is not None:
+            print(self.params[0].shape, self.params[1].shape, item_embedding.shape
+            )
             item_embedding = (item_embedding - self.params[0])/self.params[1]
         mu = self.mu(item_embedding)
         logvar = self.logvar(item_embedding)
