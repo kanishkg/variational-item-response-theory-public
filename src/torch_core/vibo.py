6d3719c2f623370e55596087f16f6ccc35f31964
Kanishk Gandhi <kanishk050296@gmail.com>
2022-01-13 13:29:49 -0800

    add feat normalize

--- src/torch_core/vibo.py
+++ src/torch_core/vibo.py
@@ -295,8 +295,8 @@ if __name__ == "__main__":
         item_embedding = embedding_model.embed_states(
             [environment.State([p], [], 0) for p in train_dataset.problems]).detach()
         print(item_embedding.shape)
-        item_mean = torch.mean(item_embedding, dim=-1)
-        item_std = torch.std(item_embedding, dim=-1)
+        item_mean = torch.mean(item_embedding, dim=0)
+        item_std = torch.std(item_embedding, dim=0)
 
 
     model = model_class(
