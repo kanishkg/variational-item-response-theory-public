c6211ecd590755ee45f72f9353b04ec9b8b9b768
Kanishk Gandhi <kanishk050296@gmail.com>
2022-01-14 18:07:06 -0800

    add simulated side-info

--- src/torch_core/vibo.py
+++ src/torch_core/vibo.py
@@ -294,7 +294,7 @@ if __name__ == "__main__":
         embedding_model = None
     side_info_model = None
     if args.side_info_model:
-        if args.side_info_mode != 'simulate':
+        if args.side_info_model != 'simulate':
             side_info_model = torch.load(args.side_info_model, map_location=device)
             side_info_model.to(device)
         else:
