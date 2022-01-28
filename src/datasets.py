c480f5ee89d5a9b36eaf72545ee7259d826cde02
Kanishk Gandhi <kanishk050296@gmail.com>
2022-01-27 18:39:16 -0800

    add duoling all

--- src/datasets.py
+++ src/datasets.py
@@ -742,14 +742,14 @@ class DuoLingo_LanguageAcquisition(torch.utils.data.Dataset):
             data_instance['format'] = format.index(instance.format)
             data_instance['prompt'] = instance.prompt
             data_instance['sentence'] = instance_to_sentence[instance.exercise_id]
+            data_instance['history'] = []
             if (instance.user, instance.token) in word_to_attempt:
                 index = bisect.bisect(word_to_attempt[(instance.user, instance.token)], instance.days)
-                if index == 0:
-                    data_instance['history'] = []
-                else:
+                if index !=0:
+                    print(instance.user, instance.token)
+                    print(word_to_response.keys())
                     data_instance['history'] = word_to_response[(instance.user, instance.token)][:index-1]
             else:
-                data_instance['history'] = []
             dataset.append(data_instance)
 
             person_ids.append(instance.user)
