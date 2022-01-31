8c0a683109cc52679420bcdca0d6c0e9c3c540ba
Kanishk Gandhi <kanishk050296@gmail.com>
2022-01-30 19:01:12 -0800

    change side-info model to generic

--- src/utils.py
+++ src/utils.py
@@ -46,7 +46,7 @@ def bernoulli_log_pdf(x, probs):
 def masked_bernoulli_log_pdf(x, mask, probs):
     dist = torch.distributions.bernoulli.Bernoulli(probs=probs)
     # log_prob = (1-probs)**0*((1-x)*0.86 + 0.14 * x) * dist.log_prob(x.relu())
-    log_prob = (1-probs)**1.5 * dist.log_prob(x.relu())
+    log_prob = dist.log_prob(x.relu())
 
     return log_prob * mask.float()
 
