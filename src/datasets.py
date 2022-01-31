ab98f426c0997c27229119147834114d0ad03d68
Kanishk Gandhi <kanishk050296@gmail.com>
2022-01-30 20:03:28 -0800

    change side-info model to generic

--- src/datasets.py
+++ src/datasets.py
@@ -1487,7 +1487,7 @@ class AbacusDataset(torch.utils.data.Dataset):
                     self.steps[i][j] = (float(student_answers[s_id][j][1])-answer_mean[j])/answer_std[j]
 
 
-        print(f'total_responses = {np.sum(self.response_mask)}, correct_responses = {np.sum(self.response)} ')
+        print(f'total_responses = {np.sum(self.response_mask)}, correct_responses = {np.sum(self.response)}, num_per_student = {np.mean(np.sum(self.response_mask, axis=1))}')
         num_train = int(0.8 * len(self.response))
         split = slice(0, num_train) if train else slice(num_train, len(self.response))
 
