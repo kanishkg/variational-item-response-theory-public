a7365206fb7b0935bccc66f22a3c9185e9166286
Kanishk Gandhi <kanishk050296@gmail.com>
2022-01-30 19:12:17 -0800

    change side-info model to generic

--- src/torch_core/models.py
+++ src/torch_core/models.py
@@ -1113,7 +1113,7 @@ class ConditionalAbilityStepInferenceNetwork(AbilityInferenceNetwork):
         item_feat_flat = item_feat_flat.view(num_person * num_item, item_feat_dim)
         step_feat_flat = step_feat.view(num_person * num_item, self.step_feat_dim)
 
-        mlp_input = torch.cat([response_flat, item_feat_flat, step_feat_flat], dim=1)
+        mlp_input = torch.cat([response_flat, item_feat_flat, 0*step_feat_flat], dim=1)
 
         return getattr(self, f'_forward_{self.ability_merge}')(
             mlp_input,
