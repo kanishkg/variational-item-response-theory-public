8318a1ae84d4b196ec45ab1a075aec9f1b8ee213
Kanishk Gandhi <kanishk050296@gmail.com>
2022-02-01 14:01:11 -0800

    add wandb

--- src/torch_core/vibo.py
+++ src/torch_core/vibo.py
@@ -747,7 +747,6 @@ if __name__ == "__main__":
                         acc = metrics['accuracy']
                         tacc = test_metrics['accuracy']
                         f.write(f'{{ "seed": {args.seed}, "model": "{model_name}","test_missing_perc": {args.test_artificial_perc}, "train_missing_perc": {args.artificial_missing_perc}, "train_accuracy": {acc}, "test_accuracy": {tacc} , "num_encode": {args.num_encode}}},\n')
-                        f.write(f'{{ "seed": {args.seed}, "model": "{model_name}","test_missing_perc": {args.test_artificial_perc}, "train_missing_perc": {args.artificial_missing_perc}, "train_accuracy": {acc}, "test_accuracy": {tacc} , "num_encode": {args.num_encode}}},\n')
                         wandb.log({
                             "train_accuracy":metrics['accuracy'], 
                             "train_auc":metrics['auroc'],
