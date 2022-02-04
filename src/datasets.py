1ce665f67af07d7d4bb7722ca844243062dd69f0
Kanishk Gandhi <kanishk050296@gmail.com>
2022-02-03 23:47:16 -0800

    debuf duo

--- src/datasets.py
+++ src/datasets.py
@@ -706,15 +706,12 @@ class DuoLingo_LanguageAcquisition(torch.utils.data.Dataset):
         self.unique_ids = unique_ids 
 
         self.steps = np.empty((self.num_person, self.num_item, self.max_history)).tolist()
-
+        self.encoder_mask = None
         for d in dataset:
             u = self.unique_ids.index(d['user'])
             i = d['token']
             h = len(['history'])
             self.steps[u, i, h] = (d['sentence']
-
-        self.dataset = dataset
-        self.encoder_mask = None
 
     def make_score_matrix(self, sub_problem, mode):
         filename = os.path.join(
