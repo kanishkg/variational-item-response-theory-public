32600d3dcdcea40d069ced2a1b79b2f056880c0e
Kanishk Gandhi <kanishk050296@gmail.com>
2022-02-18 12:59:40 -0800

    add len facts

--- src/torch_core/models.py
+++ src/torch_core/models.py
@@ -1335,7 +1335,7 @@ class ConpoleTrajectoryEncoder(nn.Module):
 
     def forward(self, steps, step_mask):
         # should be on cpu?
-        step_lens = torch.tensor([[len(prob) for prob in per] for per in steps]).to(torch.int64)
+        step_lens = torch.tensor([[len(prob.facts) for prob in per] for per in steps]).to(torch.int64)
         # empty embedding for padding: person x questions x max_len x hidden_dim
         step_embedding = torch.zeros(step_mask.size(0), step_mask.size(1), self.max_len,
                                      self.hidden_dim).to(step_mask.device)
