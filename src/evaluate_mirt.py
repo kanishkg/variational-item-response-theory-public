dce9e9f06e1e60b668664061730dcfaf4d578926
Kanishk Gandhi <kanishk050296@gmail.com>
2022-03-13 20:59:06 -0700

    debug

--- src/evaluate_mirt.py
+++ src/evaluate_mirt.py
@@ -108,6 +108,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     seed_array = list(range(args.num_seed-1)) + [42]
+    seed_array = [16, 17, 18, 20, 21]
     # number of samples for the encoder
     dataset_encode = list(range(1,11)) + [-1]
 
